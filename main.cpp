badb4852ee11a7303dfe3f0f3932904104f9d161
Jason Wu <jason.k.wu@gmail.com>
2014-10-28 02:13:28 -0400

    added some unit tests.

--- main.cpp
+++ main.cpp
@@ -14,72 +14,106 @@
 
 using namespace std;
 
-void testReadIntArray();
-void testMerge();
-void testMergeSort();
-void testReadIntArrayMergeSort();
-void testHighLow();
+typedef enum {
+	OK = 0,
+	FAIL
+} STATUS;
+
+void runUnitTests();
+int testReadIntArray();
+int testMerge();
+int testMergeSort();
+int testReadIntArrayMergeSort();
+int testHighLow();
 
 int main() {
-	testHighLow();
+	runUnitTests();
 	return 0;
 }
 
 /* unit testing */
-void testReadIntArray() {
-	In fileIn("test.txt");
+void runUnitTests() {
+	cout << "Running unit tests..." << endl;
+	
+	cout << "testReadIntArray():          ";
+	if (testReadIntArray()) cout << "FAIL" << endl;
+	else cout << "OK" << endl;
+
+	cout << "testMerge():                 ";
+	if (testMerge()) cout << "FAIL" << endl;
+	else cout << "OK" << endl;
+
+	cout << "testMergeSort():             ";
+	if (testMergeSort()) cout << "FAIL" << endl;
+	else cout << "OK" << endl;
+
+	cout << "testReadIntArrayMergeSort(): ";
+	if (testReadIntArrayMergeSort()) cout << "FAIL" << endl;
+	else cout << "OK" << endl;
+
+	cout << "testHighLow():               ";
+	if (testHighLow()) cout << "FAIL" << endl;
+	else cout << "OK" << endl;
+	
+}
+
+int testReadIntArray() {
+	In fileIn("../tests/testReadIntArray.txt");
 	int intArray[10];
+	int retval = OK;
 
 	int intCount = fileIn.readIntArray(intArray, 10);
 
-	cout << "intCount: " << intCount << endl;
 
 	for (int i = 0; i < 10; i++) {
-		cout << "line " << i << ": " << intArray[i] << endl;
+		if (intArray[i] != i + 1) retval = FAIL;
 	}
 
+	if (intCount != 10) retval = FAIL;
+
 	fileIn.close();
+
+	return retval;
 }
 
-void testMerge() {
+int testMerge() {
 	int arrL[] = { 1, 2, 4 };
 	int arrR[] = { 3, 5, 6 };
 	int arr[6];
 	int sizeL = sizeof(arrL) / sizeof(int);
 	int sizeR = sizeof(arrR) / sizeof(int);
 	int size = sizeL + sizeR;
+	int retval = OK;
 
 	memset(arr, 0, size * sizeof(int));
 
 	uint64 invCount = merge(arrL, sizeL, arrR, sizeR, arr, size, SORT_ASCEND);
 
-	cout << "merged array: ";
 	for (int i = 0; i < size; i++) {
-		cout << arr[i] << " ";
+		if (arr[i] != i + 1) retval = FAIL;
 	}
 
-	cout << endl;
+	if (invCount != 1) retval = FAIL;
 
-	cout << "inversion count: " << invCount << endl;
+	return retval;
 }
 
-void testMergeSort() {
+int testMergeSort() {
 	int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 	int size = sizeof(arr)/sizeof(int);
 	uint64 invCount = mergeSort(arr, size, SORT_ASCEND);
 
-	cout << "sorted array: ";
 	for (int i = 0; i < size; i++) {
-		cout << arr[i] << " ";
+		if (arr[i] != i + 1) return FAIL;
 	}
 
-	cout << endl;
+	if (invCount != 45) return FAIL;
 
-	cout << "inversion count: " << invCount << endl;
+	return OK;
 }
 
-void testReadIntArrayMergeSort() {
-	In fileIn("IntegerArray.txt");
+int testReadIntArrayMergeSort() {
+	In fileIn("../tests/IntegerArray.txt");
 	int arr[100000];
 
 	memset(arr, 0, 100000 * sizeof(int));
@@ -89,21 +123,28 @@ void testReadIntArrayMergeSort() {
 
 	uint64 invCount = mergeSort(arr, 100000, SORT_ASCEND);
 
-	cout << "total integers read: " << intCount << endl;
-	cout << "inversion count: " << invCount << endl;
+	if (intCount != 100000) return FAIL;
+	if (invCount != 2407905288) return FAIL;
+
+	return OK;
 }
 
-void testHighLow() {
+int testHighLow() {
 	uint16 arr[] = { 10, 4, 2, 7, 5, 6, 1, 3, 8, 9 };
 	uint16 size = sizeof(arr) / sizeof(uint16);
-
-	cout << "before: ";
-	for (uint16 i = 0; i < size; i++) cout << arr[i] << " ";
-	cout << endl;
+	bool state = true;
 
 	highlow(arr, size);
 
-	cout << "after: ";
-	for (uint16 i = 0; i < size; i++) cout << arr[i] << " ";
-	cout << endl;
+	for (int i = 0; i < size-1; i++) {
+		if (state) {
+			if (arr[i] < arr[i + 1]) return FAIL;
+		}
+		else {
+			if (arr[i] > arr[i + 1]) return FAIL;
+		}
+		state = !state;
+	}
+
+	return OK;
 }
